1a18879b602d575f9bd11afbc99e205401a26302
MeskourLhoussaine <127257257+MeskourLhoussaine@users.noreply.github.com>
2024-05-03 17:27:57 +0100

    add  editMarchand and getMerchantById and   getMarchandPaymentMethod and findStatusMerchantPayment "fonction"

--- src/app/admin/services/marchand.service.ts
+++ src/app/admin/services/marchand.service.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 import { Merchant } from '../model/merchant.model';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MarchandService {
- 
+  API_URL = environment.apiUrl;
 
-  constructor(private http:HttpClient){ }
+  constructor(private http: HttpClient) {}
 
   public getMarchands(): Observable<Array<Merchant>> {
-    return this.http.get<Array<Merchant>>(environment.apiUrl + "/api/merchants/findAll")
+    return this.http.get<Array<Merchant>>(
+      this.API_URL + '/api/merchants/findAll'
+    );
   }
 
   // public searchMarchands(keyword: string): Observable<Array<Marchand>> {
@@ -21,14 +27,62 @@ export class MarchandService {
   // }
 
   public saveMarchand(marchand: Merchant): Observable<Merchant> {
-    return this.http.post<Merchant>(environment.apiUrl + "/api/merchants/save", marchand)
+    return this.http
+      .post<Merchant>(this.API_URL + '/api/merchants/save', marchand)
+      .pipe(catchError(this.handleError));
   }
 
   public deleteMarchand(id: number): Observable<Merchant> {
-    return this.http.delete<Merchant>(environment.apiUrl + "/api/merchants/delete/" + id)
+    return this.http
+      .delete<Merchant>(this.API_URL + '/api/merchants/delete/' + id)
+      .pipe(catchError(this.handleError));
+  }
+  /*-----------using for updating marchant for Admin------------*/
+  public editMarchand(marchand: Merchant): Observable<Merchant> {
+    return this.http
+      .put<Merchant>(
+        this.API_URL + '/api/merchants/updateMarchand/' + marchand.merchantId,
+        marchand
+      )
+      .pipe(catchError(this.handleError));
   }
 
-  public editMarchand( marchand: Merchant): Observable<Merchant> {
-    return this.http.put<Merchant>(environment.apiUrl + "/api/merchants/updateMarchand/" + marchand.merchantId,marchand);
+  /*-----------using  for  morecomponenet for Admin------------*/
+  getMerchantById(id: number): Observable<Merchant> {
+    return this.http
+      .get<Merchant>(this.API_URL + '/api/merchants/findById/' + id)
+      .pipe(catchError(this.handleError));
+  }
+  /*-----------using  for  morecomponenet for Admin all methode used by marchand------------*/
+
+  getMarchandPaymentMethod(marchandId: number): Observable<Map<string, any>[]> {
+    return this.http
+      .get<Map<string, any>[]>(
+        this.API_URL + '/api/merchants/methods/' + marchandId
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  /*-----------using  for  morecomponenet for Admin Status of methods shecked by marchand------------*/
+  findStatusMerchantPayment(
+    merchantId: number,
+    paymentMethodId: number
+  ): Observable<boolean> {
+    return this.http
+      .get<boolean>(
+        this.API_URL + '/api/merchant_methods/status/' + merchantId + '/' + paymentMethodId
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, body was: ${error.error}`
+      );
+    }
+    return throwError('Something bad happened; please try again later.');
   }
 }
