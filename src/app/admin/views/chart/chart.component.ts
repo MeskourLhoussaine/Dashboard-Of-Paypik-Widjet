87122405a2aaf82601ad4484080f65d2a18c0af2
MeskourLhoussaine <127257257+MeskourLhoussaine@users.noreply.github.com>
2024-05-13 09:50:40 +0100

    commit update

---
+++ src/app/admin/views/chart/chart.component.ts
@@ -0,0 +1,138 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Transaction } from '../../model/transaction.model';
+import { TransactionService } from '../../services/transaction.service';
+
+@Component({
+  selector: 'app-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './chart.component.html',
+  styleUrl: './chart.component.css'
+})
+export class ChartComponent  implements OnInit {
+
+  @Input() marchandId!: number;
+
+  transactions: Transaction[] = [];
+
+  constructor(private transactionService: TransactionService) { // Moved the declaration of transactionService into the constructor
+  
+  }
+
+  ngOnInit(): void {
+    this.fetchTransactionsByMarchand(this.marchandId); // Fetch transactions when component initializes
+  }
+
+  ///////////// Fetch number of transactions by marchand  
+
+  totalAmount!: number;
+  previousTotalAmount: number = 0;
+  uniqueClientIds: Set<string> = new Set();
+  uniqueClientIdsLastYear: Set<string> = new Set();
+
+
+
+
+  fetchTransactionsByMarchand(marchandId: number) {
+    this.transactionService.getTransactionsByMarchand(marchandId).subscribe(
+      (data: Transaction[]) => {
+        const exchangeRates: { [key: string]: number } = {
+          'MAD': 1,
+          'EUR': 0.092701,
+          'USD': 0.1
+        };
+
+        const currentDate = new Date();
+        const previousYear = currentDate.getFullYear() - 1;
+        const previousYearStartDate = new Date(previousYear, 0, 1);
+        const previousYearEndDate = new Date(previousYear, 11, 31);
+
+        let totalMADAmount = 0;
+        let previousYearTotalMADAmount = 0;
+        
+        for (const transaction of data) {
+          const transactionDate = new Date(transaction.timestamp);
+          if (transactionDate >= previousYearStartDate && transactionDate <= previousYearEndDate) {
+            previousYearTotalMADAmount += this.convertToMAD(transaction.amount, transaction.currency, exchangeRates);
+            this.uniqueClientIdsLastYear.add(transaction.clientId); // Add client ID to set for last year
+          }
+          totalMADAmount += this.convertToMAD(transaction.amount, transaction.currency, exchangeRates);
+          this.uniqueClientIds.add(transaction.clientId); // Add client ID to set
+        }
+
+        this.previousTotalAmount = previousYearTotalMADAmount;
+        this.totalAmount = totalMADAmount;
+
+      },
+      (error) => {
+        console.error('Error fetching transactions amounts:', error);
+      }
+    );
+  }
+  
+  // Function to convert amount to MAD
+  convertToMAD(amount: number, currency: string, exchangeRates: { [key: string]: number }): number {
+    const exchangeRate = exchangeRates[currency];
+    return amount * exchangeRate;
+  }
+  
+  get totalAmounts() {
+    if (this.totalAmount !== undefined && this.totalAmount !== null) {
+      return this.totalAmount.toFixed(1);
+  } else {
+      return 'N/A'; 
+  }
+  }
+
+  get totalTransactionsByMarchand() {
+    return this.transactions.length;
+  }  
+
+  ///////////// get current year
+
+  getCurrentYear(): number {
+    return new Date().getFullYear();
+  }
+
+  //////////////// porcentage
+  calculatePercentageChange(): { value: string, isPositive: boolean } {
+  if (this.previousTotalAmount === 0) {
+    return { value: 'N/A', isPositive: true };
+  }
+
+  const percentageChange = ((this.totalAmount - this.previousTotalAmount) / this.previousTotalAmount) * 100;
+  const formattedPercentageChange = percentageChange.toFixed(1) + '%';
+
+  return {
+    value: formattedPercentageChange,
+    isPositive: percentageChange >= 0
+  };
+}
+
+////////////////////////////
+
+  calculateUniqueClientCount(): number {
+    return this.uniqueClientIds.size;
+  }
+  
+  calculateUniqueClientCountLastYear(): number {
+    return this.uniqueClientIdsLastYear.size;
+  }
+
+  calculateClientCountPercentageChange(): { value: string, isPositive: boolean } {
+    const currentYearClients = this.calculateUniqueClientCount();
+    const lastYearClients = this.calculateUniqueClientCountLastYear();
+
+    if (lastYearClients === 0) {
+      return { value: 'N/A', isPositive: true };
+    }
+
+    const percentageChange = ((currentYearClients - lastYearClients) / lastYearClients) * 100;
+    const formattedPercentageChange = percentageChange.toFixed(1) + '%';
+
+    return {
+      value: formattedPercentageChange,
+      isPositive: percentageChange >= 0
+    };
+  }
+}
--- src/app/admin/views/more/more.component.ts
+++ src/app/admin/views/more/more.component.ts
@@ -9,6 +9,8 @@ import { Merchant } from '../../model/merchant.model';
 import { MarchandService } from 'src/app/admin/services/marchand.service';
 import { PaymentMethod } from 'src/app/admin/model/payment-method.model';
 import { MethodService } from '../../services/method.service';
+import { TransactionService } from '../../services/transaction.service';
+import { Transaction } from '../../model/transaction.model';
 
 
 
@@ -21,175 +23,173 @@ Chart.register(...registerables);
   animations: [pageTransition]
 })
 export class MoreComponent implements OnInit{
-merchantId!:number;
-merchants: Merchant[] = [];
-//transactions: Transaction[] = [];
-paymentMethods: PaymentMethod[] = [];
-methods: PaymentMethod[] = [];
-// Déclaration d'un ensemble pour stocker les numéros de méthode uniques
- methodNumbers: number[] = [];
- status: boolean | undefined;
- paymentMethodId!: number;
+  marchandId!:number;
+  marchands: Merchant[] = [];
+  //transactions: Transaction[] = [];
+  paymentMethods: PaymentMethod[] = [];
+  methods: PaymentMethod[] = [];
+  // Déclaration d'un ensemble pour stocker les numéros de méthode uniques
+  methodNumbers: number[] = [];
+  status: boolean | undefined;
+  paymentMethodId!: number;
 
   constructor(
     private route: ActivatedRoute,
-    private merchantService: MarchandService,
+    private marchandService: MarchandService,
     private methodService:MethodService,
- 
+    private transitionService : TransactionService
   ) {}
-  eventDate: any = formatDate(new Date(), 'MMM dd, yyyy', 'en');
 
   ngOnInit(): void {
-    this.route.params.subscribe((params) => {
-      this.merchantId = +params['merchantId'];
-      this.paymentMethodId = +params['paymentMethodId'];
+    this.fetchMarchands();
+    this.route.params.subscribe(params => {
+      this.marchandId = params['merchantId'];
+      this.paymentMethodId = params['paymentMethodId'];
+      this.fetchTransactionsByMarchand(this.marchandId);
     Chart.register(...registerables); // Enregistrez les éléments Chart.js nécessaires
 
-    this.createChart();
-    this.retrieveMerchantById();
-   
-    this.getPaymentMethods();
-   
+    this.retrieveMarchandById();
     
-  });
+    this.getPaymentMethods();
+    });
   }
-//find marchand by id 
-retrieveMerchantById(): void {
-  this.merchantService.getMarchandById(this.merchantId).subscribe({
-    next: (data: any) => { // Utilisation de 'any' temporairement pour résoudre le problème de type
-     console.log('Merchant data:', data);
-      this.merchants.push(data as Merchant); // Cast 'data' en tant que Merchant
-     
-    },
-    error: (error) => console.error(error),
-  });
 
-}
-
-
-findStatusForMethod(method: PaymentMethod): void {
-  this.merchantService.findStatusMarchandPayment(this.merchantId, method.paymentMethodId)
-    .subscribe(status => {
-      method.status = status; // Assigner le statut à la méthode
-      console.log('Method:', method.name, 'status:', status);
-    });
-}
-
-updateStatus(paymentMethodId: number, event: any): void {
-  const checked = event.target.checked;
-  this.methodService
-    .updateMerchantMethodStatus(paymentMethodId, this.merchantId, checked)
-    .subscribe({
-      next: (response) => {
-        console.log('Status updated successfully:', response);
-        // Vous pouvez effectuer des actions supplémentaires après la mise à jour du statut si nécessaire
+  //find marchand by id 
+  retrieveMarchandById(): void {
+    this.marchandService.getMarchandById(this.marchandId).subscribe({
+      next: (data: any) => { // Utilisation de 'any' temporairement pour résoudre le problème de type
+      console.log('Marchand data:', data);
+        this.marchands.push(data as Merchant); // Cast 'data' en tant que Marchand
+    
       },
-      error: (error) => {
-        console.error('Error updating status:', error);
-        // Gérer l'erreur si la mise à jour échoue
-      },
+      error: (error) => console.error(error),
     });
-}
-getPaymentMethods(): void {
-  this.methodService.getAll().subscribe(
-    (data) => {
-      this.paymentMethods = data;
-      
-      console.log('les methodes pay:', this.paymentMethods);
-      
-      // Call findStatusForMethod for each payment method
-      this.paymentMethods.forEach((method) => {
-        this.findStatusForMethod(method);
+  }
+  
+  
+  findStatusForMethod(method: PaymentMethod): void {
+    this.marchandService.findStatusMarchandPayment(this.marchandId, method.paymentMethodId)
+      .subscribe(status => {
+        method.status = status; // Assigner le statut à la méthode
+        console.log('Method:', method.name, 'status:', status);
       });
-    },
-    (error) => {
-      console.error('Error fetching unverified demandes:', error);
-    }
-  );
-}
-
-/*
-
-getMethodsUsingByMarchand(): void {
-  this.merchantService.getMarchandPaymentMethod(this.merchantId).subscribe({
-    next: (data: any[]) => {
-      for (let i = 0; i < data.length; i++) {
-        const methodData = data[i];
-        const method: PaymentMethod = {
-          paymentMethodId: methodData.paymentMethodId,
-          name: methodData.name,
-          description: methodData.description,
-          iconUrl: methodData.iconUrl
-        };
-        this.methods.push(method); // Ajoute la méthode à la liste des méthodes
-        this.methodNumbers.push(method.paymentMethodId); 
-        this.findStatusForMethod(method);// Ajoute le paymentMethodId à la liste des identifiants de méthode
+  }
+  
+  updateStatus(paymentMethodId: number): void {
+    this.methodService
+      .updateMarchandMethodStatus(paymentMethodId, this.marchandId)
+      .subscribe({
+        next: () => {
+          console.log('Status updated successfully');
+          // Vous pouvez effectuer des actions supplémentaires après la mise à jour du statut si nécessaire
+        },
+        error: (error) => {
+          console.error('Error updating status:',error);
+          // Gérer l'erreur si la mise à jour échoue
+        },
+      });
+  }
+  getPaymentMethods(): void {
+    this.methodService.getAll().subscribe(
+      (data) => {
+        this.paymentMethods = data;
+        
+        console.log('les methodes pay:', this.paymentMethods);
+        
+        // Call findStatusForMethod for each payment method
+        this.paymentMethods.forEach((method) => {
+          this.findStatusForMethod(method);
+        });
+      },
+      (error) => {
+        console.error('Error fetching unverified demandes:', error);
       }
-      console.log('Merchant paymentMethode', this.methods);
-      console.log('Array of method IDs: ', this.methodNumbers);
- 
-   
-    },
-    error: (error) => console.error(error),
-  });
-
- 
-}*/
-
-isMethodChecked(method: PaymentMethod): boolean {
-  return this.methods.some(m => m.paymentMethodId === method.paymentMethodId && m.status === true);
-}
-
-/*update implement */
-
-
-  ////////////Scroll to section //////////////
-
-  @ViewChild('detailedDescription') detailedDescription!: ElementRef;
-
-  scrollToSection() {
-    if (this.detailedDescription && this.detailedDescription.nativeElement) {
-      this.detailedDescription.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    );
+  }
+  
+  
+  isMethodChecked(method: PaymentMethod): boolean {
+    return this.methods.some(m => m.paymentMethodId === method.paymentMethodId && m.status === true );
+  }
+  
+  /*update implement */
+  
+  
+    ////////////Scroll to section //////////////
+  
+    @ViewChild('detailedDescription') detailedDescription!: ElementRef;
+  
+    scrollToSection() {
+      if (this.detailedDescription && this.detailedDescription.nativeElement) {
+        this.detailedDescription.nativeElement.scrollIntoView({ behavior: 'smooth' });
+      }
     }
-  }
-// test chart 
+    //////////////////////////////
 
-  //-----------------------------chart ---------------------
-  createChart() {
-    const ctx = document.getElementById('areaWiseSale') as HTMLCanvasElement;
-    const myChart = new Chart(ctx, {
-      type: 'doughnut',
-      data: {
-        labels: ['Token', 'Card', 'Epay', 'Paypal'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-          ],
-        }]
-      },
-      options: {
-        scales: {
-          x: {
-            display: false
-          },
-          y: {
-            display: false
-          },
+
+    get getmarchandId() {
+      return this.marchandId;
+    }
+  
+    marchand: Merchant | undefined;
+  
+    fetchMarchands() {
+      this.marchandService.getMarchands().subscribe(
+        (data: Merchant[]) => {
+          this.marchands = data;
+          
+          const Id = this.marchandId; 
+  
+          this.marchand = this.marchands.find(marchand => marchand.merchantId === Number(Id));
+          if (!this.marchand) {
+            console.error('Marchand not found with id:', Id);
+          }
         },
-        plugins: {
-          legend: {
-            position: 'right',
-            align: 'center',
-          },
+        (error) => {
+          console.error('Error fetching marchands:', error);
+        }
+      );
+    }
+
+    ///////////// Fetche number of transaction by marchand  
+    transactions: Transaction[]=[];
+  
+    fetchTransactionsByMarchand(marchandId:number) {
+      this.transitionService.getTransactionsByMarchand(marchandId).subscribe(
+        (data: Transaction[]) => {
+          this.transactions = data;
         },
-      },
-    });
-  }
+        (error) => {
+          console.error('Error fetching transactions:', error);
+        }
+      );
+    }
+
+    get totalTransactionsByMarchand() {
+      return this.transactions.length;
+    }  
+
+    get thisMarchand() {
+      return this.marchand;
+    } 
+    /////////////  Fetch marchand by id 
+    // fetchMarchandByid(marchandId :number) {
+    //   this.marchandService.getMarchandById(marchandId).subscribe(
+    //     (data: Marchand[]) => {
+    //       this.marchands = data;
+          
+    //       const Id = this.marchandId; 
+  
+    //       this.marchand = this.marchands.find(marchand => marchand.marchandId === Number(Id));
+    //       if (!this.marchand) {
+    //         console.error('Marchand not found with id:', Id);
+    //       }
+    //     },
+    //     (error) => {
+    //       console.error('Error fetching marchands:', error);
+    //     }
+    //   );
+    // }
 }
 
 
